13eac583fdaf8375de41daa1cb0d41c0f1df7c96
Shubham Yadav <shubhamlaxmanyadav2@gmail.com>
2026-09-21 22:34:55 +0530

    pro

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,6 @@ import { useState } from 'react';
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -11,4 +10,4 @@ function App() {
   )
 }
 
-export default App
+export default App;
